0f44957a9522c1e584c1d22f25bb1146df50600f
jeromewjk <39673094+jeromewjk@users.noreply.github.com>
2018-07-30 19:08:18 +0800

    5th

--- client/main.js
+++ client/main.js
@@ -26,23 +26,20 @@ Router.configure({
 
 Template.eventList.helpers({
     'searchedEvents' : function() {
-        console.log('before function');
         return searchEvents();
     }
-//
-//        var list = [];
-//        for (var i = 0; i < localStorage.length; i++) {
-//            if (localStorage.getItem(localStorage.key(i)) == null) {
-//                break;
-//            }
-//            list = list.concat(JSON.parse(localStorage.getItem(localStorage.key(i))));
-//        }
-//        return list;
-//    }
 });
 
+Template.eventList.onRendered(function() {
+     $(".loginButton").removeClass('selected');
+     $(".calendarButton").removeClass('selected');
+     $(".newEventButton").removeClass('selected');
+     $(".moduleListButton").removeClass('selected');
+     $(".registerButton").removeClass('selected');
+     $(".eventListButton").addClass('selected');
+ });
+
 function searchEvents() {
-    // console.log('searchEvents called');
      var list = [];
         for (var i = 0; i < localStorage.length; i++) {
             // console.log('inside for loop');
@@ -52,33 +49,78 @@ function searchEvents() {
             // console.log(JSON.parse(localStorage.getItem(localStorage.key(i))));
             list = list.concat(JSON.parse(localStorage.getItem(localStorage.key(i))));
         }
-//        localStorage.clear();
-//        console.log('localStorage cleared');
         return list;
 };
 
+Template.listModules.onRendered(function() {
+     $(".loginButton").removeClass('selected');
+     $(".calendarButton").removeClass('selected');
+     $(".newEventButton").removeClass('selected');
+     $(".moduleListButton").addClass('selected');
+     $(".registerButton").removeClass('selected');
+     $(".eventListButton").removeClass('selected');
+ });
+
+Template.listModules.events({
+    'submit form' : function(event){
+        event.preventDefault();
+        var deleteModule = $('[name="deleteModuleEvents"]').val().toUpperCase();
+        if(typeof Events.find({module: deleteModule}).fetch()[0] !== 'undefined'){
+            $('[name="deleteModuleEvents"]').val("");
+            var confirm = window.confirm("Delete all events related to module " + deleteModule + "?");
+            if(confirm){
+                Events.find({module: deleteModule}).forEach(function(doc){
+                    Events.remove({_id: doc._id});
+                });
+            };
+        } else {
+            sAlert.warning("No events created for this module.", {timeout: 5000, onRouteClose: true});
+        }
+    },
+});
+
 Template.listModules.helpers({
     'item' : function() {
-        //event.preventDefault();
         var currentUser = Meteor.userId();
         var stuff = Events.find(
         {
             createdBy: currentUser,
             startDate: { $gte: new Date() }
-        }).fetch();
-        // console.log(stuff);
+        }, {sort: {module:1, startDate:1}}).fetch();
+        var displayArray = testing();
         return stuff;
     },
-
-    'checked' : function() {
-        var isCompleted = this.completed;
-        if (isCompleted) {
-            return "checked";
-        } else {
-            return "";
-        }
-    }
 });
+
+function testing(){
+    var currentUser = Meteor.userId();
+     var array = Events.find(
+        {
+            createdBy: currentUser,
+            startDate: { $gte: new Date() }
+        }, {sort: {module:1, startDate:1}}).fetch();
+
+     var finalArr = [];
+     var currentArr = [];
+     currentArr.push(array[0]);
+     for (var i = 1; i < array.length; i++) {
+        if(array[i].module == array[i-1].module){
+            currentArr.push(array[i])
+        } else {
+            finalArr.push(currentArr);
+            currentArr=[];
+            currentArr.push(array[i]);
+        }
+        if(i+1 == array.length){
+            finalArr.push(currentArr);
+        }
+    };
+    // console.log(finalArr);
+    return finalArr;
+    // document.getElementById("p3").innerHTML = currentArr[0].module;
+}
+
+
 
 Template.module.events({
     'click .delete-event' : function() {
@@ -102,8 +144,6 @@ Template.module.events({
                     if (confirm) {
                         array.splice(j, 1);
                         localStorage.setItem(key, array);
-                        // console.log('module deleted');
-                        console.log(array);
                         return;
                     }
                 }
@@ -113,16 +153,28 @@ Template.module.events({
 });
 
 Template.newEvent.onRendered(function(){
+    $(".loginButton").removeClass('selected');
+    $(".calendarButton").removeClass('selected');
+    $(".newEventButton").addClass('selected');
+    $(".moduleListButton").removeClass('selected');
+    $(".registerButton").removeClass('selected');
+    $(".eventListButton").removeClass('selected');
+
     $('.newEvent').validate({
         submitHandler : function() {
             var currentUser = Meteor.userId();
-            var module = $('[name="module"]').val();
+            var module = $('[name="module"]').val().toUpperCase();
             var date = event.target.date.value;
             var month = event.target.month.value;
             var year = event.target.year.value;
             var startTime = event.target.startTime.value;
+            if(startTime < 10){
+                startTime = "0" + startTime;
+            }
             var endTime = event.target.endTime.value;
-            // var type = event.target.type.value;
+            if(endTime < 10){
+                endTime = "0" + endTime;
+            }
             var type = document.getElementById("dropList").value;
             var description = event.target.description.value;
             var id = event.target.id.value;
@@ -153,12 +205,13 @@ Template.newEvent.onRendered(function(){
             if (!error) {
                 Events.insert({
                     id: id,
-                    module: module.toUpperCase(),
+                    module: module,
                     date: date,
                     month: month,
                     year: year,
                     startTime: startTime,
                     endTime: endTime,
+
                     type: type,
                     description: description,
                     createdBy: currentUser,
@@ -167,14 +220,11 @@ Template.newEvent.onRendered(function(){
                     title: module + " " + type,
                     completed: true
                 });
-                var print = ("Event Created!\n" +
-                    "Module: " + module + "\n" +
-                    "Date: " + date + "/" + month + "/" + year + "\n" +
-                    "From: " + startTime + "00 To: " + endTime + "00\n" +
-                    "Type: " + type + "\n" +
-                    "Description: " + description);
-                alert(print);
+                var localList = Events.find({module: module}).fetch();
+                localStorage.setItem(module, JSON.stringify(localList));
                 Router.go('home');
+                sAlert.warning("Event created!" + module +".", {timeout: 5000, onRouteClose: true});
+
             }
         }
     })
@@ -201,6 +251,13 @@ Template.login.events({
 });
 
 Template.login.onRendered(function() {
+    $(".loginButton").addClass('selected');
+    $(".calendarButton").removeClass('selected');
+    $(".newEventButton").removeClass('selected');
+    $(".moduleListButton").removeClass('selected');
+    $(".registerButton").removeClass('selected');
+    $(".eventListButton").removeClass('selected');
+
     var validator = $('.login').validate({
         submitHandler: function(event) {
             var email = $('[name=email]').val();
@@ -229,6 +286,13 @@ Template.login.onRendered(function() {
 });
 
 Template.register.onRendered(function() {
+    $(".loginButton").removeClass('selected');
+    $(".calendarButton").removeClass('selected');
+    $(".newEventButton").removeClass('selected');
+    $(".moduleListButton").removeClass('selected');
+    $(".registerButton").addClass('selected');
+    $(".eventListButton").removeClass('selected');
+
     var validator = $('.register').validate({
         submitHandler : function(event) {
             var email = $('[name=email]').val();
@@ -260,16 +324,16 @@ $.validator.setDefaults({
         password: {
             minlength: 6
         },
-        // startTime: {
-        //     digits: true,
-        //     max: 23,
-        //     min: 00
-        // },
-        // endTime: {
-        //     digits: true,
-        //     max: 23,
-        //     min: 00
-        // },
+        startTime: {
+            digits: true,
+            max: 23,
+            min: 00
+        },
+        endTime: {
+            digits: true,
+            max: 23,
+            min: 00
+        },
         month: {
             min: 1,
             max: 12
@@ -348,44 +412,38 @@ Template.calendar.events({
                     color1 = "#6aa87b";
                 } else {
                     color1 = "#fd254f";
-                    sAlert.success(array[i].title + "\n due on : " + array[i].date +"/"+ array[i].month+"/" + array[i]. year,
-                     {effect: 'genie', position: 'bottom-right',
-                      timeout: 8000, onRouteClose: true, stack: true, offset: '10px'});
-                }
 
-//                var tempObj = {
-//                    id: i,
-//                    title: array[i].title,
-//                    module: array[i].module,
-//                    start: array[i].startDate,
-//                    type: array[i].type,
-//                    description: array[i].description,
-//                    createdBy: array[i].createdBy,
-//                    completed: array[i].completed
-//                    color: #asdj
-//                }
+                    var print = array[i].title;
+                    if(array[i].type == "Quiz" || array[i].type == "Assignment"){
+                        print += "<br>Due on: " + moment(array[i].startDate).format("Do MMM YY") +
+                        ", " + moment(array[i].startDate).format("hh:mm A");
+                      } else {
+                        print += "<br>Happening on: " + moment(array[i].startDate).format("Do MMM YY") +
+                        ", " + moment(array[i].startDate).format("hh:mm A");
+                    }
+                    sAlert.warning(print,{effect: 'genie', position: 'bottom-right',
+                    timeout: 8000, onRouteClose: true, stack: true, offset: '10px', html:true});
+                }
 
                 $('#calendarDiv').fullCalendar('renderEvent', {
                     id: i,
                     title: array[i].title,
                     module: array[i].module,
                     start: array[i].startDate,
+                    end: array[i].endDate,
                     type: array[i].type,
                     description: array[i].description,
                     createdBy: array[i].createdBy,
                     completed: array[i].completed,
                     color: color1,
                 }, true);
-
-//               newArr.push(tempObj);
             }
-            // console.log(newArr);
             console.log("Success!");
         }
     });
 
 function SearchAndParse (moduleName) {
-     var newArr=[];
+    var newArr=[];
     var keyCount = 0;
     for (keyCount = 0; keyCount < localStorage.length; keyCount++){
 
@@ -393,42 +451,49 @@ function SearchAndParse (moduleName) {
         var array = Events.find({module: moduleSearch}).fetch();
 
         for (i = 0; i < array.length; i++) {
-        // console.log(array[i].startDate);
-         var currentDate = array[i].startDate;
-         var todayDate = new Date();
-        var day = 1000*60*60*24;
-        var diff = Math.ceil((todayDate.getTime()-currentDate.getTime())/(day));
-        var color1;
+            var currentDate = array[i].startDate;
+            var todayDate = new Date();
+            var day = 1000*60*60*24;
+            var diff = Math.ceil((todayDate.getTime()-currentDate.getTime())/(day));
+            var color1;
 
-        if(diff > 0){
-                    color1 = "#011f4b";
-                } else if (diff<-6){
-                    color1 = "#0099ff";
-                } else if (diff < -2){
-                    color1 = "#6aa87b";
+            if(diff > 0){
+                color1 = "#011f4b";
+            } else if (diff<-6){
+                color1 = "#0099ff";
+            } else if (diff < -2){
+                color1 = "#6aa87b";
+            } else {
+                color1 = "#fd254f";
+
+                var print = array[i].title;
+                if(array[i].type == "Quiz" || array[i].type == "Assignment"){
+                    print += "<br>Due on: " + moment(array[i].startDate).format("Do MMM YY") +
+                    ", " + moment(array[i].startDate).format("hh:mm A");
                 } else {
-                    color1 = "#fd254f";
-                    sAlert.success(array[i].title + "\n due on : " + array[i].date +"/"+ array[i].month+"/" + array[i]. year,
-                     {effect: 'genie', position: 'bottom-right',
-                      timeout: 8000, onRouteClose: true, stack: true, offset: '10px'});
+                    print += "<br>Happening on: " + moment(array[i].startDate).format("Do MMM YY") +
+                    ", " + moment(array[i].startDate).format("hh:mm A");
                 }
-        var tempObj = {
-            id: i,
-            title: array[i].title,
-            module: array[i].module,
-            start: array[i].startDate,
-            type: array[i].type,
-            description: array[i].description,
-            createdBy: array[i].createdBy,
-            completed: array[i].completed,
-            color: color1
+                sAlert.warning(print,{effect: 'genie', position: 'bottom-right',
+                    timeout: 8000, onRouteClose: true, stack: true, offset: '10px', html:true});
+            }
+            var tempObj = {
+                id: i,
+                title: array[i].title,
+                module: array[i].module,
+                start: array[i].startDate,
+                end: array[i].endDate,
+                type: array[i].type,
+                description: array[i].description,
+                createdBy: array[i].createdBy,
+                completed: array[i].completed,
+                color: color1
+            }
+            newArr.push(tempObj);
         }
-        newArr.push(tempObj);
-        }
-        // console.log("im runningggg");
-};
+    };
+
     $('#calendarDiv').fullCalendar({
-        // themeSystem: 'bootstrap3',
         header: {
             left: 'agendaDay, agendaWeek, month',
             center: 'title',
@@ -437,28 +502,26 @@ function SearchAndParse (moduleName) {
         slotDuration: '00:30:00',
         editable: false,
         events: newArr,
-
            eventClick: function(event) {
                  var print = ("Event title: " + event.title +
-                     "\nModule: " + event.module +
-                     "\nDescription: " + event.description +
-                     "\nDate & Time: " + moment(event.start).format("Do MMM YY") +
-                      ", " + moment(event.start).format("hh:mm A"));
-                     // "\nEvent Date: " + event.date + "/"event.month+"/"event.year +
-                     //"\nEvent time: " + moment(event.start).format("hh:mm A"));
-                 if(event.end){
-                     alert(print + " to " + moment(event.end).format("hh:mm A"));
-                 } else {
-                 //alert(print);
-                var print = JSON.parse(JSON.stringify(print));
-                // console.log(print);
-                sAlert.close(sAlert.warning());
-                sAlert.warning(print
-                    ,{effect: 'scale', position: 'top-right', timeout: 'none',onRouteClose: true, stack: true, offset: '80px'});
-            };
-            console.log(alert._id);
+                     "<br>Module: " + event.module +
+                     "<br>Date & Time: " + moment(event.start).format("Do MMM YY") +
+                      ", " + moment(event.start).format("hh:mm A") + 
+                      " to " + moment(event.end).format("hh:mm A"));
+                 if(event.description !== ""){
+                    print = print + "<br>Description: " + event.description;
+                 };                    
+                 sAlert.warning(print,{effect: 'scale', position: 'top-right', timeout: 'none',onRouteClose: true, stack: true, offset: '80px', html: true});
             },
         });
      }
 
+     Template.calendar.onRendered(function() {
+     $(".loginButton").removeClass('selected');
+     $(".calendarButton").addClass('selected');
+     $(".newEventButton").removeClass('selected');
+     $(".moduleListButton").removeClass('selected');
+     $(".registerButton").removeClass('selected');
+     $(".eventListButton").removeClass('selected');
+ });
      Template.calendar.onRendered(() => SearchAndParse(""));
